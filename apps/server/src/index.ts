978d414cb1b3327ccc602f898c5ce33e2e7a7e08
Josef Bauer <josef.bauer.1st@gmail.com>
2026-08-31 13:47:23 +0200

    Change log output

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,7 +2,7 @@ import { app } from "./app";
 
 const port = Number(process.env.PORT ?? 3000);
 
-console.log(`steelmind server listening on http://localhost:${port}`);
+console.log(`Steelmind server listening on http://localhost:${port}`);
 
 export default {
   port,
